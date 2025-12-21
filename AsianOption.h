68c617527bdba68a762466c6fa8f906ead86e961
emem-spec <emma.bekker@edu.devinci.fr>
2025-12-21 17:15:27 +0100

    Add comments for clarity in AsianOption class
    Added comments to clarify constructor, getter, and override methods.

--- AsianOption.h
+++ AsianOption.h
@@ -10,15 +10,17 @@ protected:
 
 public:
 
+    // constructor
     explicit AsianOption(const std::vector<double>& path);
 
-
+    // getter timesteps
     const std::vector<double>& getTimeSteps() const;
 
- 
+     // ovveride payoffpath
     double payoffPath(const std::vector<double>& spotPath) const override;
 
-
+    // Override bool isAsianOption 
     bool isAsianOption() const override;
 };
+
 
