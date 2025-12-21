0968e3f820e40a12757885958ca78718e728e498
emem-spec <emma.bekker@edu.devinci.fr>
2025-12-21 17:13:23 +0100

    Refactor AsianCallOption class implementation

--- AsianCallOption.cpp
+++ AsianCallOption.cpp
@@ -1,12 +1,16 @@
 #include "AsianCallOption.h"
 
+// Constructor
 AsianCallOption::AsianCallOption(const std::vector<double>& path, double strike)
     : AsianOption(path), _strike(strike)
 {
     if (strike < 0.0)
+    {
         throw std::invalid_argument("AsianCallOption: strike must be >= 0");
+    }
 }
 
+// Override payoff
 double AsianCallOption::payoff(double avgSpot) const
 {
     if (avgSpot > _strike)
@@ -16,8 +20,10 @@ double AsianCallOption::payoff(double avgSpot) const
     return 0.0;
 }
 
+// getter strike
 double AsianCallOption::getStrike() const
 {
     return _strike;
 
 }
+
