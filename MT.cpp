c8e67913c8d2873b368600e2a028a7084b773130
emem-spec <emma.bekker@edu.devinci.fr>
2025-12-21 16:52:51 +0100

    Enhance comments for clarity in MT.cpp
    Added comments to clarify the purpose of methods and distributions.

--- MT.cpp
+++ MT.cpp
@@ -5,6 +5,8 @@
 std::mt19937 MT::generator;
 bool MT::initialized = false;
 
+
+// This method checks that the generator has been initialized only once and initializes its seed with the current time to ensure we get different sequences of numbers for different times
 void MT::initialize()
 {
     if (!initialized)
@@ -16,6 +18,7 @@ void MT::initialize()
     }
 }
 
+// to generate numbers following a uniform distribution
 double MT::rand_unif()
 {
     initialize();  // Ensure generator is initialized
@@ -25,6 +28,7 @@ double MT::rand_unif()
     return distribution(generator);
 }
 
+// to generate numbers following a normal distribution
 double MT::rand_norm()
 {
     initialize();  // Ensure generator is initialized
@@ -32,4 +36,5 @@ double MT::rand_norm()
     // Normal distribution N(0, 1)
     std::normal_distribution<double> distribution(0.0, 1.0);
     return distribution(generator);
+
 }
