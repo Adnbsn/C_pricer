ebc7c22a77985c47e5f8fa575a590c195829bf56
emem-spec <emma.bekker@edu.devinci.fr>
2025-12-21 17:16:59 +0100

    Refactor AsianPutOption implementation

--- AsianPutOption.cpp
+++ AsianPutOption.cpp
@@ -1,12 +1,16 @@
 #include "AsianPutOption.h"
 
+// constructor
 AsianPutOption::AsianPutOption(const std::vector<double>& path, double strike)
     : AsianOption(path), _strike(strike)
 {
     if (strike < 0.0)
+    {
         throw std::invalid_argument("AsianPutOption: strike must be >= 0");
+    }
 }
 
+// override payoff
 double AsianPutOption::payoff(double avgSpot) const
 {
     if (_strike > avgSpot)
@@ -16,8 +20,10 @@ double AsianPutOption::payoff(double avgSpot) const
     return 0.0;
 }
 
+// getter for strike
 double AsianPutOption::getStrike() const
 {
     return _strike;
 
 }
+
