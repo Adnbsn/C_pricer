9e385901e40ec5e1ed693f7c78dbc8066ccd341e
emem-spec <emma.bekker@edu.devinci.fr>
2025-12-21 16:56:19 +0100

    Fix formatting and clean up AsianCallOption.h

--- AsianCallOption.h
+++ AsianCallOption.h
@@ -9,11 +9,11 @@ private:
     double _strike;
 
 public:
-    // FIX: constructor uses const reference
+    
     AsianCallOption(const std::vector<double>& path, double strike);
 
-    // FIX: payoff must use avgSpot, not "strike"
     double payoff(double avgSpot) const override;
 
     double getStrike() const;
 };
+
